5f8a23a83a6008d9689fa04fa5d02f67647d264c
ETIM-PAUL <etimpaul22@gmail.com>
2022-02-04 23:39:42 +0100

    first commit - adding google maps

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react'; 
+import { Autocomplete } from '@react-google-maps/api';
+import {AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core';
+
+//icons
+import SearchIcon from '@material-ui/icons/Search';
+
+//styles
+import useStyles from './styles'
+
+
+const Header = () => {
+  const classes = useStyles();
+
+  
+  return (
+    <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+            <Typography variant="h4" className={classes.title}>
+                Traveller Help
+            </Typography>
+            <Box display="flex">
+                <Typography variant ="h6" className={classes.title}>
+                  Maximum Satisfaction Explorer
+                </Typography>
+                {/* <Autocomplete> */}
+                  <div className={classes.search}>
+                    <div className = {classes.searchIcon}>
+                      <SearchIcon />
+                    </div>
+                    <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+                  </div>
+                {/* </Autocomplete> */}
+            </Box>
+        </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
